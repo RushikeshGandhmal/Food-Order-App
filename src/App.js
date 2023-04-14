226c73ba736afbd588dd9792361fd61d981c1266
Rushi <rushikeshgandhmal@gmail.com>
2023-04-14 16:16:33 +0530

    added Card and MealItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Fragment } from "react";
 import "./App.css";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 };
